f28485b9cf6cc5c1717cf7085cfcd20aca5caee2
Ankit Kumar <mail.ankitkumar14@gmail.com>
2024-02-24 23:40:22 +0530

    Category Page

--- digiasset_frontend/pages/index.js
+++ digiasset_frontend/pages/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
 import Style from '../styles/index.module.css';
-import { HeroSection,Service,BigNFTSlider,Subscribe } from '../components/componentsindex';
+import { HeroSection,Service,BigNFTSlider,Subscribe,Title, Category } from '../components/componentsindex';
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
       <Service />
       <BigNFTSlider />
+      <Title heading="Browse by Category" paragraph="Explore the NFTs in the most featured Categories"/>
+      <Category />
       <Subscribe />
     </div>
   )
