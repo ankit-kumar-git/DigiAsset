4209b27ef4f0bf249cc8eb4ec31c6043c50aecee
Ankit Kumar <mail.ankitkumar14@gmail.com>
2024-02-24 23:58:04 +0530

    Filter Page Added

--- digiasset_frontend/pages/index.js
+++ digiasset_frontend/pages/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
 import Style from '../styles/index.module.css';
-import { HeroSection,Service,BigNFTSlider,Subscribe,Title, Category } from '../components/componentsindex';
+import { HeroSection,Service,BigNFTSlider,Subscribe,Title, Category, Filter } from '../components/componentsindex';
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
       <Service />
       <BigNFTSlider />
+      <Title heading="Featured NFTs" paragraph="Discover the most outstanding NFTs of all topics of life"/>
+      <Filter />
       <Title heading="Browse by Category" paragraph="Explore the NFTs in the most featured Categories"/>
       <Category />
       <Subscribe />
