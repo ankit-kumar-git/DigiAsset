90d4f5079a7960756ae6ba3947eca0deb82cf8be
Ankit Kumar <mail.ankitkumar14@gmail.com>
2024-02-25 01:18:56 +0530

    collection page added

--- digiasset_frontend/pages/index.js
+++ digiasset_frontend/pages/index.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React from "react";
 
-import Style from '../styles/index.module.css';
-import { HeroSection,Service,BigNFTSlider,Subscribe,Title, Category, Filter, NFTCard } from '../components/componentsindex';
+import Style from "../styles/index.module.css";
+import {
+  HeroSection,
+  Service,
+  BigNFTSlider,
+  Subscribe,
+  Title,
+  Category,
+  Filter,
+  NFTCard,
+  Collection,
+} from "../components/componentsindex";
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
       <Service />
       <BigNFTSlider />
-      <Title heading="Featured NFTs" paragraph="Discover the most outstanding NFTs of all topics of life"/>
+      <Title
+        heading="New Collections"
+        paragraph="Discover the most outstanding NFTs of all topics of life"
+      />
+      <Collection />
+      <Title
+        heading="Featured NFTs"
+        paragraph="Discover the most outstanding NFTs of all topics of life"
+      />
       <Filter />
       <NFTCard />
-      <Title heading="Browse by Category" paragraph="Explore the NFTs in the most featured Categories"/>
+      <Title
+        heading="Browse by Category"
+        paragraph="Explore the NFTs in the most featured Categories"
+      />
       <Category />
       <Subscribe />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
